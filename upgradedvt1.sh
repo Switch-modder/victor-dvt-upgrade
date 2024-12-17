fc5bfe50582694c5b0779efcfe18aca8a7781f82
Raj-jyot Singh Deol <deolrajjyot@gmail.com>
2024-12-16 21:31:05 -0500

    Update upgradedvt1.sh
    Recovery partition added

--- upgradedvt1.sh
+++ upgradedvt1.sh
@@ -33,24 +33,30 @@ echo "Get parted"
 curl -o /system/dvtupgrade/parted http://wire.my.to:81/parted
 chmod +rwx /system/dvtupgrade/parted
 echo "Curl/Flash..."
-SMALL_DISPLAY "System_a"
+SMALL_DISPLAY "System_a" #The main VicOS
 echo "System_a"
 curl -o /system/dvtupgrade/sysfs.img.gz http://wire.my.to:81/dvt2cfwsystem.img.gz
-SMALL_DISPLAY "Boot_a"
+SMALL_DISPLAY "Boot_a" #The thing to boot VicOS
 echo "Boot_a"
 curl -o /system/dvtupgrade/boot.img.gz http://wire.my.to:81/dvt2cfwboot.img.gz
-SMALL_DISPLAY "EMR"
+SMALL_DISPLAY "EMR" #Calibration data
 echo "EMR"
 curl -o /system/dvtupgrade/emr.img http://wire.my.to:81/006emr.img
-SMALL_DISPLAY "OEM"
+SMALL_DISPLAY "OEM" #Serial number and stuff
 echo "OEM"
 curl -o /system/dvtupgrade/oem.img http://wire.my.to:81/006oem.img
-SMALL_DISPLAY "Aboot"
+SMALL_DISPLAY "Aboot" #No firmware signing boi
 echo "Aboot"
-curl -o /system/dvtupgrade/aboot.img http://wire.my.to:81/dvt2aboot.img
-SMALL_DISPLAY "Modem"
+curl -o /system/dvtupgrade/aboot.img http://wire.my.to:81/ankidev-nosigning.mbn
+SMALL_DISPLAY "Modem" #Internet and bluetooth stuff
 echo "Modem"
 curl -o /system/dvtupgrade/modem.img http://wire.my.to:81/dvt2modem.img
+SMALL_DISPLAY "Modem" #Internet and bluetooth stuff
+echo "recoveryfs"
+curl -o /system/dvtupgrade/recoveryfs.img.gz http://wire.my.to:81/dumps/devrecoveryfs.img.gz
+SMALL_DISPLAY "Modem" #Internet and bluetooth stuff
+echo "recovery"
+curl -o /system/dvtupgrade/recovery.img.gz http://wire.my.to:81/dumps/devrecovery.img.gz
 
 #Confirm everything is good with a file and hash check
 BIG_DISPLAY "Check files"
@@ -124,78 +130,10 @@ else
 fi
 sleep 5
 
-SMALL_DISPLAY "Check System_a"
-echo "Checking if System_a exists"
-# Define the expected hash
-EXPECTED_HASH_A="614bc7e96aaee5bf12ae363cafe5f759"
-
-# Check if the file exists
-if [ ! -f /system/dvtupgrade/sysfs.img.gz ]; then
-    echo "System_a does not exist. Confirm System_a is on the server or download it to your bot manually."
-    exit 0
-else
-    echo "System_a is found. Proceeding to check the file hash."
-
-    # Compute the hash of the file
-    if command -v md5sum >/dev/null 2>&1; then
-        ACTUAL_HASH_A=$(md5sum /system/dvtupgrade/sysfs.img.gz | awk '{print $1}')
-    else
-        echo "Error: md5sum command not found. Please install it to proceed."
-        exit 1
-    fi
-
-    # Compare the hash values
-    if [ "$ACTUAL_HASH_A" = "$EXPECTED_HASH_A" ]; then
-        echo "File hash matches the expected value."
-    else
-        echo "Error: File hash does NOT match the expected value!"
-        echo "Expected: $EXPECTED_HASH_A"
-        echo "Actual:   $ACTUAL_HASH_A"
-        exit 1
-    fi
-    #Clear hash
-    unset ACTUAL_HASH_A
-fi
-sleep 5
-
-SMALL_DISPLAY "Check Boot_a"
-echo "Checking if Boot_a exists"
-# Define the expected hash
-EXPECTED_HASH_BOOT="614bc7e96aaee5bf12ae363cafe5f759"
-
-# Check if the file exists
-if [ ! -f /system/dvtupgrade/boot.img.gz ]; then
-    echo "Boot_a does not exist. Confirm Boot_a is on the server or download it to your bot manually."
-    exit 0
-else
-    echo "Boot_a is found. Proceeding to check the file hash."
-
-    # Compute the hash of the file
-    if command -v md5sum >/dev/null 2>&1; then
-        ACTUAL_HASH_BOOT=$(md5sum /system/dvtupgrade/boot.img.gz | awk '{print $1}')
-    else
-        echo "Error: md5sum command not found. Please install it to proceed."
-        exit 1
-    fi
-
-    # Compare the hash values
-    if [ "$ACTUAL_HASH_BOOT" = "$EXPECTED_HASH_BOOT" ]; then
-        echo "File hash matches the expected value."
-    else
-        echo "Error: File hash does NOT match the expected value!"
-        echo "Expected: $EXPECTED_HASH_BOOT"
-        echo "Actual:   $ACTUAL_HASH_BOOT"
-        exit 1
-    fi
-    #Clear hash
-    unset ACTUAL_HASH_BOOT
-fi
-sleep 5
-
 SMALL_DISPLAY "Check EMR"
 echo "Checking if EMR exists"
 # Define the expected hash
-EXPECTED_HASH_EMR="614bc7e96aaee5bf12ae363cafe5f759"
+EXPECTED_HASH_EMR="a6553e7b223b12a85809c957cfd3173c"
 
 # Check if the file exists
 if [ ! -f /system/dvtupgrade/emr.img ]; then
@@ -229,7 +167,7 @@ sleep 5
 SMALL_DISPLAY "Check OEM"
 echo "Checking if OEM exists"
 # Define the expected hash
-EXPECTED_HASH_OEM="614bc7e96aaee5bf12ae363cafe5f759"
+EXPECTED_HASH_OEM="54322f44b65e0e1db020ebacdf4f757f"
 
 # Check if the file exists
 if [ ! -f /system/dvtupgrade/oem.img ]; then
@@ -260,6 +198,142 @@ else
 fi
 sleep 5
 
+SMALL_DISPLAY "Check ABOOT"
+echo "Checking if ABOOT exists"
+# Define the expected hash
+EXPECTED_HASH_ABOOT="1b447f29bcca755638ebbef56068aedf"
+
+# Check if the file exists
+if [ ! -f /system/dvtupgrade/aboot.img ]; then
+    echo "ABOOT does not exist. Confirm ABOOT is on the server or download it to your bot manually."
+    exit 0
+else
+    echo "ABOOT is found. Proceeding to check the file hash."
+
+    # Compute the hash of the file
+    if command -v md5sum >/dev/null 2>&1; then
+        ACTUAL_HASH_ABOOT=$(md5sum /system/dvtupgrade/aboot.img | awk '{print $1}')
+    else
+        echo "Error: md5sum command not found. Please install it to proceed."
+        exit 1
+    fi
+
+    # Compare the hash values
+    if [ "$ACTUAL_HASH_ABOOT" = "$EXPECTED_HASH_ABOOT" ]; then
+        echo "File hash matches the expected value."
+    else
+        echo "Error: File hash does NOT match the expected value!"
+        echo "Expected: $EXPECTED_HASH_ABOOT"
+        echo "Actual:   $ACTUAL_HASH_ABOOT"
+        exit 1
+    fi
+    #Clear hash
+    unset ACTUAL_HASH_ABOOT
+fi
+sleep 5
+
+SMALL_DISPLAY "Check Modem"
+echo "Checking if Modem exists"
+# Define the expected hash
+EXPECTED_HASH_MODEM="bf26f360023283b06bb5f50d650b9e48"
+
+# Check if the file exists
+if [ ! -f /system/dvtupgrade/modem.img ]; then
+    echo "Modem does not exist. Confirm Modem is on the server or download it to your bot manually."
+    exit 0
+else
+    echo "Modem is found. Proceeding to check the file hash."
+
+    # Compute the hash of the file
+    if command -v md5sum >/dev/null 2>&1; then
+        ACTUAL_HASH_MODEM=$(md5sum /system/dvtupgrade/modem.img | awk '{print $1}')
+    else
+        echo "Error: md5sum command not found. Please install it to proceed."
+        exit 1
+    fi
+
+    # Compare the hash values
+    if [ "$ACTUAL_HASH_MODEM" = "$EXPECTED_HASH_MODEM" ]; then
+        echo "File hash matches the expected value."
+    else
+        echo "Error: File hash does NOT match the expected value!"
+        echo "Expected: $EXPECTED_HASH_MODEM"
+        echo "Actual:   $ACTUAL_HASH_MODEM"
+        exit 1
+    fi
+    #Clear hash
+    unset ACTUAL_HASH_MODEM
+fi
+sleep 5
+
+SMALL_DISPLAY "Check recoveryfs"
+echo "Checking if recoveryfs exists"
+# Define the expected hash
+EXPECTED_HASH_RFS="8d3e92b5aed4b26fbb6b8554030fb5b0 "
+
+# Check if the file exists
+if [ ! -f /system/dvtupgrade/recoveryfs.img.gz ]; then
+    echo "recoveryfs does not exist. Confirm recoveryfs is on the server or download it to your bot manually."
+    exit 0
+else
+    echo "recoveryfs is found. Proceeding to check the file hash."
+
+    # Compute the hash of the file
+    if command -v md5sum >/dev/null 2>&1; then
+        ACTUAL_HASH_RFS=$(md5sum /system/dvtupgrade/recoveryfs.img.gz | awk '{print $1}')
+    else
+        echo "Error: md5sum command not found. Please install it to proceed."
+        exit 1
+    fi
+
+    # Compare the hash values
+    if [ "$ACTUAL_HASH_RFS" = "$EXPECTED_HASH_RFS" ]; then
+        echo "File hash matches the expected value."
+    else
+        echo "Error: File hash does NOT match the expected value!"
+        echo "Expected: $EXPECTED_HASH_RFS"
+        echo "Actual:   $ACTUAL_HASH_RFS"
+        exit 1
+    fi
+    #Clear hash
+    unset ACTUAL_HASH_RFS
+fi
+sleep 5
+
+SMALL_DISPLAY "Check recovery"
+echo "Checking if recovery exists"
+# Define the expected hash
+EXPECTED_HASH_REC="2f0ce78e70db21271974cf1fb7115439"
+
+# Check if the file exists
+if [ ! -f /system/dvtupgrade/recovery.img.gz ]; then
+    echo "recovery does not exist. Confirm recovery is on the server or download it to your bot manually."
+    exit 0
+else
+    echo "recovery is found. Proceeding to check the file hash."
+
+    # Compute the hash of the file
+    if command -v md5sum >/dev/null 2>&1; then
+        ACTUAL_HASH_REC=$(md5sum /system/dvtupgrade/recovery.img.gz | awk '{print $1}')
+    else
+        echo "Error: md5sum command not found. Please install it to proceed."
+        exit 1
+    fi
+
+    # Compare the hash values
+    if [ "$ACTUAL_HASH_REC" = "$EXPECTED_HASH_REC" ]; then
+        echo "File hash matches the expected value."
+    else
+        echo "Error: File hash does NOT match the expected value!"
+        echo "Expected: $EXPECTED_HASH_REC"
+        echo "Actual:   $ACTUAL_HASH_REC"
+        exit 1
+    fi
+    #Clear hash
+    unset ACTUAL_HASH_REC
+fi
+sleep 5
+
 echo "Erasing Switchboard"
 /system/dvtupgrade/busybox dd if=/dev/zero of=/dev/block/bootdevice/by-name/sbl1bak count=1024
 echo "Kill processes"
@@ -268,10 +342,6 @@ sleep 5
 echo "Umount data"
 echo 1 1 w Still going | /system/bin/display > /dev/null
 umount /data
-BIG_DISPLAY "System_a..."
-/system/dvtupgrade/busybox gzip -dc /system/dvtupgrade/sysfs.img.gz | dd of=/dev/block/bootdevice/by-name/userdata
-BIG_DISPLAY "Boot_a..."
-/system/dvtupgrade/busybox gzip -dc /system/boot.img.gz | dd of=/dev/block/bootdevice/by-name/boot
 BIG_DISPLAY "EMR..."
 /system/dvtupgrade/busybox dd if=/system/dvtupgrade/emr.img of=/dev/block/bootdevice/by-name/cache
 BIG_DISPLAY "OEM..."
@@ -280,13 +350,17 @@ BIG_DISPLAY "Aboot..."
 /system/dvtupgrade/busybox dd if=/system/dvtupgrade/aboot.img of=/dev/block/bootdevice/by-name/aboot
 BIG_DISPLAY "Modem..."
 /system/dvtupgrade/busybox dd if=/system/dvtupgrade/modem.img of=/dev/block/bootdevice/by-name/modem
+BIG_DISPLAY "recovery"
+/system/dvtupgrade/busybox gzip -dc /system/dvtupgrade/recovery.img.gz | dd of=/dev/block/bootdevice/by-name/persist
+BIG_DISPLAY "recoveryfs"
+/system/dvtupgrade/busybox gzip -dc /system/dvtupgrade/recoveryfs.img.gz | dd of=/dev/block/bootdevice/by-name/userdata
 echo "Rename partitions"
-echo "system to recoveryfs"
-SMALL_DISPLAY "system to recoveryfs"
-/system/dvtupgrade/parted /dev/block/mmcblk0 name 21 recoveryfs
-SMALL_DISPLAY "userdata to system_a"
-echo "userdata to system_a"
-/system/dvtupgrade/parted /dev/block/mmcblk0 name 28 system_a
+echo "system to system_a"
+SMALL_DISPLAY "system to system_a"
+/system/dvtupgrade/parted /dev/block/mmcblk0 name 21 system_a
+SMALL_DISPLAY "userdata to recoveryfs"
+echo "userdata to recoveryfs"
+/system/dvtupgrade/parted /dev/block/mmcblk0 name 28 recoveryfs
 echo "boot to boot_a"
 SMALL_DISPLAY "boot to boot_a"
 /system/dvtupgrade/parted /dev/block/mmcblk0 name 20 boot_a
